4980ab003013d042ced8c8c050626b90a9efb147
Amirhossein Mottie <zen43zem@rptu.de>
2026-06-15 14:44:48 +0200

    Load data

---
+++ src/00_rawdata.R
@@ -0,0 +1,7 @@
+data1 <- read.csv("./data/aneuploid_growth_data1.csv")
+data2 <- read.csv("./data/aneuploid_growth_data2.csv")
+data3 <- read.csv("./data/aneuploid_growth_data3.csv")
+
+head(data1)
+head(data2)
+head(data3)
